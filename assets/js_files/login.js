1686a831b994ce447d65c7cebcaecf1046d8b385
Bhargav Borse <bhargavborse4898@gmail.com>
2020-07-19 09:41:19 +0530

    updated successfully. no errors in this update

--- assets/js_files/login.js
+++ assets/js_files/login.js
@@ -27,8 +27,7 @@ firebase.auth().onAuthStateChanged(function (user) {
             signInOptions: [
                 // Leave the lines as is for the providers you want to offer your users.
                 firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-                firebase.auth.EmailAuthProvider.PROVIDER_ID,
-                firebase.auth.PhoneAuthProvider.PROVIDER_ID
+                firebase.auth.EmailAuthProvider.PROVIDER_ID
             ],
             // Terms of service url.
             tosUrl: 'tos.html',
